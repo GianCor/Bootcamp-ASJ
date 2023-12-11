6e472e9c1d9ca196c8fa3d42cdad633038c3caf3
unknown <gcornet@petersen.corp>
2023-12-11 17:36:55 -0300

    Ultimos ejercicios de JavaScript

---
+++ 20231211-CORNET-UltimosEjerciciosJs/pruebasFetch/index.js
@@ -0,0 +1,11 @@
+let joke = document.querySelector("h2");
+let button = document.getElementById("button");
+button.addEventListener("click", ()=>{
+    fetch(`https://api.chucknorris.io/jokes/random`)
+  .then((response) => response.json()) //convertir a JSON
+  .then((json) => {
+    console.log(json.value);
+    joke.innerText = json.value;
+  })
+  .catch((error) => console.log("Ocurrió un error: " + error));
+})
