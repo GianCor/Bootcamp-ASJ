8f1661e76a06822801d421a6a9b706af1782aaec
unknown <gcornet@petersen.corp>
2023-12-13 09:13:44 -0300

    Primer proyecto en angular (Tardé en subir porque tuve problemas para que lo reconozca el repositorio)

---
+++ 20231212-CORNET-Angular/my-app/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardComponent } from './components/card/card.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { SearchbarComponent} from './components/searchbar/searchbar.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    PricesComponent,
+    CarouselComponent,
+    SearchbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
